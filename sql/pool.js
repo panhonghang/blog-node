d01bdda4ee9a67c7cf92df2d856a3ec4592fa4d5
panhonghang <44355254+panhonghang@users.noreply.github.com>
2020-02-20 16:32:17 +0800

    Update pool.js

--- sql/pool.js
+++ sql/pool.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   host     : '47.100.167.231',
   user     : 'root',
-  password : '9872014skylove',
+  password : '****',
   database : 'blog'
 });
  
